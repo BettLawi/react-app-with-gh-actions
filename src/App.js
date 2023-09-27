6f934d4f682245801959d065fb67028f5902c90f
BettLawi <lawibett06@gmail.com>
2023-09-27 11:36:57 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           <h1>TESTING GITHUB PAGES WITH REACT AND GITHUB ACTIONS</h1>
-//           <h2>This is a second test</h2>
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 
 function App() {
