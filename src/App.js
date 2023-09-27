51360d8a625b096d649f67a0f6c3b6857722eeab
BettLawi <lawibett06@gmail.com>
2023-09-27 11:34:06 +0300

    react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           <h1>TESTING GITHUB PAGES WITH REACT AND GITHUB ACTIONS</h1>
+//           <h2>This is a second test</h2>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const handleTaskInputChange = (e) => {
+    setTaskText(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskText.trim() === '') {
+      return;
+    }
+    setTasks([...tasks, taskText]);
+    setTaskText('');
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>TESTING GITHUB PAGES WITH REACT AND GITHUB ACTIONS</h1>
-          <h2>This is a second test</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple To-Do App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={taskText}
+          onChange={handleTaskInputChange}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
